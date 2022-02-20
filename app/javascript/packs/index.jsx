ba7be264d7dd8e1c418e9a1e115fd3fdfb2bdeec
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-02-19 21:57:16 -0800

    React router setup

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -6,10 +6,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import App from '../components/app'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>,
     document.body.appendChild(document.createElement('div'))
   )
 })
